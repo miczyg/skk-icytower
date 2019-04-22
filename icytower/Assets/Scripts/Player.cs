26717b99d20c5b2d14b88abd79c0f99b4f823daf
miczyg <miczyg13@gmail.com>
2019-04-22 12:36:29 +0200

    basic scene

---
+++ icytower/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Public
+    public float upForce = 200f;
+
+    #endregion Public
+
+    #region Private
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    #endregion Private
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // If the bird collides with something set it to dead...
+        isDead = true;
+    }
+}
